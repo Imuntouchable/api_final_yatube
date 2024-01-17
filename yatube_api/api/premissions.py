75c889702ca2a1618552e8b9062283f54a73ec11
Александр <sasatoguzov@gmail.com>
2024-01-17 19:19:51 +0300

    API

--- yatube_api/api/premissions.py
+++ yatube_api/api/premissions.py
@@ -10,6 +10,7 @@ class CreatorOrReadOnly(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        if request.method in ['GET', 'HEAD', 'OPTION']:
-            return request.method in permissions.SAFE_METHODS
-        return obj.author == request.user
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
